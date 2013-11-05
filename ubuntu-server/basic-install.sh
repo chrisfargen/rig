d440e1f34b8e995909f0e72c6d74fb91f29e06ad
Chris Fargen <chrisfargen@gmail.com>
2013-11-05 20:02:34 +0000

    Moved install files to own directory

---
+++ ubuntu-server/basic-install.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# http://stackoverflow.com/a/192337/1351736
+me="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+# Shortcut to path
+git_url="https://raw.github.com/chrisfargen/rig/master"
+
+echo "** Attempting to log..."
+echo -e "$(date +'%F %R')\t$me\tJust checkin in!" >> log
+
+# SYSTEM STUFF
+
+#echo "** Creating new user..."
+
+#echo "** Changing host name..."
+
+echo "** Attempting to update system..."
+apt-get update && apt-get upgrade -y
+
+echo "** Attempting to install packages..."
+apt-get install vim git nginx -y
+
+# VIM STUFF
+
+echo "** Attempting to download minimal vim config..."
+wget -N $git_url/.vimrc
+
+# WEB SERVER STUFF
+
+echo "** Attempting to download fargen site manager..."
+wget $git_url/fargen-site.sh -O /usr/local/bin/fargen-site
+wget -N $git_url/fargen-vhost.conf -P /etc/nginx
+wget -N $git_url/basic-vhost -P /etc/nginx/sites-available
+
+echo "** Setting permissions on script..."
+chmod -v +x ~/bin/fargen-site.sh
+
+echo "** Disable default enabled site..."
+rm /etc/nginx/sites-enabled/*
+
+echo "** Changing ownership on document root..."
+chown -Rv ubuntu:ubuntu /usr/share/nginx/www
+
+echo "** Making link to document root..."
+ln -sv /usr/share/nginx/www /var/www
+
+echo "** Hurray!"
+
+exit 0
---
+++ ubuntu-server/run-setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# http://stackoverflow.com/a/192337/1351736
+me="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
+#
+git_url="https://raw.github.com/chrisfargen/rig/master"
+
+echo "** Attempting to download script..."
+wget -N $git_url/basic-install.sh -P ~/bin
+
+echo "** Setting script permissions..."
+chmod -v +x ~/bin/basic-install.sh
+
+echo "** Attempting to execute script..."
+bash ~/bin/basic-install.sh
+
+echo "** Attempting to log..."
+echo -e "$(date +'%F %R')\t$me\tJust checkin in!" >> log
+
+exit 0
