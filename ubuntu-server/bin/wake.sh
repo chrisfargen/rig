fece711b87fa5052c26acce56c33dcea984ac6ae
Christopher Fargen <chrisfargen@gmail.com>
2013-11-17 23:49:05 -0500

    Re-added shutdown and wake scripts

---
+++ ubuntu-server/bin/wake.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo echo $(date '+%s' -d '10am') | sudo tee /sys/class/rtc/rtc0/wakealarm
+sudo echo $(date '+%s' -d '5pm') | sudo tee /sys/class/rtc/rtc0/wakealarm
+#sudo shutdown -h now
+
+exit 0
