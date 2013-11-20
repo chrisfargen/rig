7da580c86d6fe7a5ac53f70da48591e44c9b3106
Christopher Fargen <chrisfargen@gmail.com>
2013-11-20 01:24:29 -0500

    Added fargen-note

---
+++ lubuntu-client/bin/fargen-note.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# fargen-note.sh
+
+notes_dir="$HOME/Dropbox/Notes"
+month_dir=$notes_dir/`date +%Y/%m`
+day_file=$month_dir/`date +%d`.md
+
+# echo "** Running fargen note manager..."
+
+if [ -z "$1" ]
+then
+    echo "** Opening today's note..."
+    vi $day_file
+elif [ "$1" = "last" ]
+then
+    # echo "** Retrieving last note..."
+    find $notes_dir -type f -printf '%T@ %p\n' | sort -n | tail -1 | cut -f2- -d" "
+elif [ "$1" = "j" ]
+then
+    # journal entry
+    vi $day_file
+elif [ "$1" = "add" ]
+then
+    # echo "** Create note"
+    # Create directory corresponding to current date
+    mkdir -p $month_dir
+    
+    # Append header for current time
+    #cat >> $day_file <<-NewEntry
+
+
+## `date +%R`
+
+
+#NewEntry
+
+    # Open document in editor
+    vim + $day_file -c "$ r ! echo -e '\n\#\# '`date +\%R`'\n\n'"
+
+else
+    echo "** What do you want to do?"
+fi
+
+echo "** Hurray!"
+
+exit 0
