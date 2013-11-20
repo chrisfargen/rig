652656fe77625ee522fd85b1d392856b80852d04
Christopher Fargen <chrisfargen@gmail.com>
2013-11-19 21:41:42 -0500

    Uncommented echos

--- ubuntu-server/bin/install-ruby.sh
+++ ubuntu-server/bin/install-ruby.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
-# echo "** Installing curl..."
+echo "** Installing known deps..."
 sudo apt-get install curl nodejs
 
-# echo "** Installing RVM..."
+echo "** Installing RVM..."
 curl -L https://get.rvm.io | bash -s stable
 
-# echo "** Checking for RVM requirements..."
+echo "** Sourcing RVM..."
+. ~/.rvm/scripts/rvm
+
+echo "** Checking for RVM requirements..."
 rvm requirements
 
-# echo "** Installing Ruby..."
+echo "** Installing Ruby..."
 rvm install ruby
 
-# echo "** Setting default Ruby version..."
+echo "** Setting default Ruby version..."
 rvm use ruby --default
 
-# echo "** Installing RubyGems..."
+echo "** Installing RubyGems..."
 rvm rubygems current
 
-# echo "** Installing Rails..."
+echo "** Installing Rails..."
 gem install rails
 
 echo "** Hurray!"
