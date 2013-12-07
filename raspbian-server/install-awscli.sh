6d6bdd6e719d266f90f268f4ca9862c52a97b458
Christopher Fargen <chrisfargen@gmail.com>
2013-12-06 20:09:25 -0500

    ok

--- raspbian-server/install-awscli.sh
+++ raspbian-server/install-awscli.sh
@@ -15,3 +15,10 @@ wget https://s3.amazonaws.com/aws-cli/awscli-bundle.zip
 unzip awscli-bundle.zip
 
 sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+
+echo "
+AWS Access Key ID [None]: 
+AWS Secret Access Key [None]: 
+Default region name [None]: us-west-2
+Default output format [None]: json
+" | sudo tee -a ~/.aws/config
