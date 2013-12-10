b685b4667f0351ee359b0e6891e9d4a6442fa3d1
Ubuntu <ubuntu@ip-10-232-143-99.us-west-2.compute.internal>
2013-12-10 15:19:19 +0000

    Make rig better

--- raspbian-server/make-server.sh
+++ raspbian-server/make-server.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # $1 is intended host name
-# $2 is elastic ip address
-# $3 is name of key pair
+# $2 is name of key pair
+# $3 is elastic ip address
 
 # Time to wait for instance to initiate
 instance_wait="90s"
 
 # Path to key file
-key_path=$(echo ~)/.ssh/$3.pem
+key_path=$(echo ~)/.ssh/$2
 
 # http://stackoverflow.com/a/192337/1351736
 me="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
@@ -19,8 +19,8 @@ git_url="https://raw.github.com/chrisfargen/rig/master"
 # Outputting variables
 cat <<EOM
 ** Intended host name: $1
-** Elastic IP address: $2
-** Name of key pair: $3
+** Elastic IP address: $3
+** Name of key pair: $2
 ** Path to key: $key_path
 EOM
 
@@ -30,15 +30,15 @@ echo "** Attempting to run instance..."
 last_instance_id=$(\
     aws ec2 run-instances \
     --image-id "ami-6aad335a" \
-    --key-name $3 \
+    --key-name $2 \
     --instance-type "t1.micro" \
     --query "Instances[0].InstanceId" \
     | sed -e 's/^"//'  -e 's/"$//')
 
 echo "** Instance id: $last_instance_id"
 
-echo "** Attempting to associate address $2..."
-aws ec2 associate-address --instance-id $last_instance_id --public-ip $2 --output text
+echo "** Attempting to associate address $3..."
+aws ec2 associate-address --instance-id $last_instance_id --public-ip $3 --output text
 
 echo "** Attempting to create name tag $1..."
 aws ec2 create-tags --resources $last_instance_id --tags Key=Name,Value=$1 --output text
@@ -46,8 +46,8 @@ aws ec2 create-tags --resources $last_instance_id --tags Key=Name,Value=$1 --out
 #echo "** Attempting to terminate instance $last_instance_id..."
 #aws ec2 terminate-instances --instance-ids $last_instance_id --output text
 
-echo "** Clearing host fingerprint for address $2..."
-ssh-keygen -f ~/.ssh/known_hosts -R $2
+echo "** Clearing host fingerprint for address $3..."
+ssh-keygen -f ~/.ssh/known_hosts -R $3
 
 echo "** Clearing host fingerprint for address $1..."
 ssh-keygen -f ~/.ssh/known_hosts -R $1
@@ -64,7 +64,7 @@ echo "** Setting script permissions..."
 chmod -v +x ~/bin/retry-ssh.sh
 
 echo "** Attempting to execute script..."
-~/bin/retry-ssh.sh "-i $key_path ubuntu@$2"
+~/bin/retry-ssh.sh "-i $key_path ubuntu@$3"
 
 # wget / chmod
 
@@ -80,9 +80,9 @@ sed -i "s/hostname.example1.com/$1/g" ~/bin/basic-install.sh
 # ssh
 
 echo "** Attemping to SCP script to server..."
-scp -i $key_path ~/bin/basic-install.sh ubuntu@$2:~/basic-install
+scp -i $key_path ~/bin/basic-install.sh ubuntu@$3:~/basic-install
 
 echo "** Attempting to SSH into server..."
-ssh -i $key_path ubuntu@$2
+ssh -i $key_path ubuntu@$3
 
 exit 0
