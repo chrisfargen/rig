8c1ec79288b4aea92bba898ddfd2d9a752791adb
Christopher Fargen <chrisfargen@gmail.com>
2013-12-10 10:03:41 -0500

    Confirm awscli install instructions

--- raspbian-server/install-awscli.sh
+++ raspbian-server/install-awscli.sh
@@ -2,6 +2,14 @@
 
 # http://docs.aws.amazon.com/cli/latest/userguide/cli-chap-getting-set-up.html
 
+# echo "** Start instance..."
+
+# echo "** SSH into instance..."
+
+cd ~
+
+sudo apt-get update && sudo apt-get dist-upgrade -y
+
 sudo apt-get install zip -y
 
 wget https://s3.amazonaws.com/aws-cli/awscli-bundle.zip
@@ -13,5 +21,7 @@ sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
 mkdir -p ~/.aws
 
 #echo "** Copy file '.aws/config' from dropbox"
+
+#touch ~/.aws/config
 
 #echo "** Run 'aws ec2 describe-instances'"
