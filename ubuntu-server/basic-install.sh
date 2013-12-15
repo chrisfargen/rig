b66b2fc3bddde9e35db0961d4be064f9bbf661cf
Christopher Fargen <chrisfargen@gmail.com>
2013-12-15 12:47:05 -0500

    ok

--- ubuntu-server/basic-install.sh
+++ ubuntu-server/basic-install.sh
@@ -18,34 +18,30 @@ echo "** Install packages..."
 sudo apt-get install vim git nginx mysql-server anacron php5-mysql curl zip -y
 
 #echo "** Attempting to install autoupdate script..."
-#
 
 # USER SETUP
 
-# in ~/.bashrc, uncomment line '#force_color_prompt=yes'
-# and log in again
-
-echo "Copying ssh key to '/etc/skel/'..."
+echo "** Copy ssh key to '/etc/skel/'..."
 sudo cp -r $HOME/.ssh /etc/skel
 
 echo "** Input new user..."
 read new_user
 
-echo "** Creating new user '$new_user'..."
+echo "** Create new user '$new_user'..."
 sudo adduser $new_user --gecos ""
 
-echo "** Creating new group 'web'..."
+echo "** Create new group 'web'..."
 sudo addgroup web
 
-echo "** Adding user '$new_user' to group 'web'..."
+echo "** Add user '$new_user' to group 'web'..."
 sudo adduser $new_user web
 sudo adduser ubuntu web
 
-echo "** Adding user '$new_user' to sudoers..."
+echo "** Add user '$new_user' to sudoers..."
 echo "$new_user ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/$new_user
 sudo chmod -v 440 /etc/sudoers.d/$new_user
 
-echo "** Changing host name to '$host_name'..."
+echo "** Change host name to '$host_name'..."
 sudo hostname $host_name
 echo $host_name | sudo tee /etc/hostname
 echo -e "127.0.1.1\t$host_name" | sudo tee -a /etc/hosts
@@ -53,53 +49,57 @@ echo -e "127.0.1.1\t$host_name" | sudo tee -a /etc/hosts
 # TODO: COME BACK HERE 12/13
 
 echo "** Make link to document root..."
-sudo ln -sv -T /usr/share/nginx/www $dr
+sudo ln -s -v -T /usr/share/nginx/www $dr
 
 echo "** Clone rig..."
 cd /var/www ; git clone $rig_url
 
 # VIM STUFF
 
-echo "** Attempting to download minimal vim config..."
-sudo ln -s -T $dr/rig/ubuntu-server/lib/.vimrc /home/$new_user
+echo "** Attempt to download minimal vim config..."
+sudo ln -s -v -T /var/www/rig/ubuntu-server/lib/.vimrc /home/$new_user/
 
+# .profile
 echo "export EDITOR=/usr/bin/vi" | sudo tee -a /home/$new_user/.profile
 echo "export VISUAL=/usr/bin/vi" | sudo tee -a /home/$new_user/.profile
 
+# .bashrc
+echo "force_color_prompt=yes" | sudo tee -a /home/$new_user/.bashrc
+
 # WEB SERVER STUFF
 
-echo "** Attempting to download fargen site manager..."
-sudo ln -s -T $dr/rig/ubuntu-server/bin/fargen-site.sh /usr/local/bin/fargen-site
-sudo ln -s -T $dr/rig/ubuntu-server/bin/unlock.sh /usr/local/bin/unlock
-sudo ln -s $dr/rig/ubuntu-server/lib/fargen-vhost.conf /etc/nginx/
-sudo ln -s $dr/rig/ubuntu-server/lib/basic-vhost /etc/nginx/sites-available
-sudo ln -s $dr/rig/ubuntu-server/lib/robots.txt /usr/share/nginx/
+echo "** Attempt to download fargen site manager..."
+sudo ln -s -v -T $dr/rig/ubuntu-server/bin/fargen-site.sh /usr/local/bin/fargen-site
+sudo ln -s -v -T $dr/rig/ubuntu-server/bin/unlock.sh /usr/local/bin/unlock
+sudo ln -s -v $dr/rig/ubuntu-server/lib/fargen-vhost.conf /etc/nginx/
+sudo ln -s -v $dr/rig/ubuntu-server/lib/basic-vhost /etc/nginx/sites-available/
+sudo ln -s -v $dr/rig/ubuntu-server/lib/robots.txt /usr/share/nginx/
 
-echo "** Creating nginx site config..."
+echo "** Create nginx site config..."
 sed "s/hostname.example2.com/$host_name/g" /etc/nginx/sites-available/basic-vhost | sudo tee /etc/nginx/sites-available/basic-vhost
 
 sudo sed "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g" /etc/php5/fpm/php.ini | sudo dd of=/etc/php5/fpm/php.ini
 
 sed "s/listen = 127.0.0.1:9000/listen = \/var\/run\/php5-fpm.sock/g" /etc/php5/fpm/pool.d/www.conf | sudo dd of=/etc/php5/fpm/pool.d/www.conf
 
-echo "** Setting permissions on script..."
+echo "** Set permissions on script..."
 sudo chmod -v +x /usr/local/bin/fargen-site /usr/local/bin/unlock
 
 echo "** Disable default enabled site..."
 sudo rm -v /etc/nginx/sites-enabled/*
 
-echo "** Setting up 'rig' directory for latest scripts..."
+echo "** Set up 'rig' directory for latest scripts..."
 cd $dr && sudo git clone https://github.com/chrisfargen/rig.git
 
 echo "** Organize vhost skel..."
-sudo mkdir -pv $dr/basic-vhost/htdocs
-sudo cp $dr/index.html $dr/basic-vhost/htdocs
-sudo cp $dr/robots.txt $dr/basic-vhost/htdocs
+sudo mkdir -p -v $dr/basic-vhost/htdocs/
+sudo cp -v $dr/index.html $dr/basic-vhost/htdocs/
+sudo cp -v $dr/robots.txt $dr/basic-vhost/htdocs/
 
-echo "** Changing ownership on document root..."
-sudo chown -Rv :web $dr/
+echo "** Change ownership on document root..."
+sudo chown -R -v :web $dr/
 
-echo "** Changing permissions on document root..."
+echo "** Change permissions on document root..."
 # find $dr -type f -exec chmod ug=rw,o=r {} \;
 # find $dr -type d -exec chmod ug=rws,o=rx {} \;
 sudo find $dr \
@@ -116,7 +116,7 @@ do
 done;
 read -p "** Press any key to continue...")
 
-echo "** MySQL install continuing..."
+echo "** Continue MySQL install ..."
 sudo mysql_install_db
 
 sudo mysql_secure_installation
@@ -143,7 +143,7 @@ then
     echo "<?php phpinfo() ?>" | sudo tee /var/www/bourbon/htdocs/index.php
 
 else
-    echo "** Service nginx not enabled."
+    echo "** Test site not enabled."
 fi
 
 echo "** Hurray!"
