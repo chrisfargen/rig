cdb1f74435af83c8aa89975cd8ab5e5908997545
Christopher Fargen <chrisfargen@gmail.com>
2013-12-28 17:11:11 -0500

    Add help to fargen-note

--- lubuntu-client/bin/fargen-note.sh
+++ lubuntu-client/bin/fargen-note.sh
@@ -2,40 +2,47 @@
 # fargen-note.sh
 
 notes_dir="$HOME/Dropbox/Notes"
-month_dir=$notes_dir/$(date +%Y/%m)
-day_file=$month_dir/$(date +%d).md
+year_dir="$notes_dir/$(date +%Y)"
+day_file="$year_dir/$(date +%F).md"
 
-# echo "** Running fargen note manager..."
-
-if [ -z "$1" ]
+if [ -z "$1" -o "$1" = "j" -o "$1" = "journal" ]
 then
     echo "** Opening today's note..."
-    mkdir -p $month_dir
-    vi + $day_file
-elif [ "$1" = "dir" ]
+    # Create directory corresponding to current date
+    mkdir -p $year_dir
+    vi + "$day_file"
+elif [ "$1" = "d" -o "$1" = "dir" ]
 then
-    echo $month_dir
+    echo $year_dir
 elif [ "$1" = "last" ]
 then
-    # echo "** Retrieving last note..."
     find $notes_dir -type f -printf '%T@ %p\n' | sort -n | tail -1 | cut -f2- -d" "
-elif [ "$1" = "j" ]
+elif [ "$1" = "add" -o "$1" = "new" ]
 then
-    # journal entry
-    vi $day_file
-elif [ "$1" = "add" ]
-then
-    # echo "** Creating note..."
-    # Create directory corresponding to current date
-    mkdir -p $month_dir
-    
+    if [ -n "$2" ]
+    then
+        echo "** Creating note..."
+        mkdir -p $year_dir
+        vi + "$year_dir/$2"
+    else
+	echo "** Warning: No note name specified!"
+	vi +"lcd $year_dir"
+    fi
     # Open document in editor
-    vim + $day_file -c "$ r ! echo -e '\n\#\# '`date +\%R`'\n\n'"
-
+    #vim + $day_file -c "$ r ! echo -e '\n\#\# '`date +\%R`'\n\n'"
+elif [ "$1" = "help" ]
+then
+    cat <<EOM
+	Options:
+	j, journal	    Open markdown document for today's date in editor
+	d, dir		    Output path to current year's note directory
+	last		    Output path to last edited note
+	add [title],	    Add note with given title to current year's note directory
+	    new [title]	    
+	help		    Display help
+EOM
 else
     echo "** What do you want to do?"
 fi
-
-echo "** Hurray!"
 
 exit 0
